cd77cd128b754392cb861d77ca8b51610819f47e
Gokhan Kurt <krtgkn@gmail.com>
2021-12-05 03:58:43 +0300

    implement extension logic

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,59 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { dirname, resolve } from 'path';
+import * as TJS from 'typescript-json-schema';
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ts-json-schema" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ts-json-schema.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ts-json-schema!');
-	});
+export function activate({ subscriptions }: vscode.ExtensionContext) {
+  const customScheme = 'tsschema';
 
-	context.subscriptions.push(disposable);
+  const watchFiles: { [key: string]: vscode.Uri } = {};
+
+  const onSave = (e: vscode.TextDocument) => {
+    const relativePath = vscode.workspace.asRelativePath(e.uri);
+    const uri = watchFiles[relativePath];
+    if (uri) {
+      tsschemaProvider.onDidChangeEmitter.fire(uri);
+    }
+  };
+
+  const tsschemaProvider = new class implements vscode.TextDocumentContentProvider {
+    onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
+    onDidChange = this.onDidChangeEmitter.event;
+
+    async provideTextDocumentContent(uri: vscode.Uri): Promise<string> {
+      const path = uri.authority + uri.path;
+      const type = uri.fragment;
+
+      const settings: TJS.PartialArgs = {
+        required: true,
+      };
+
+      const compilerOptions: TJS.CompilerOptions = {
+        strictNullChecks: true,
+      };
+
+      const currentUri = vscode.window.activeTextEditor?.document.uri;
+
+      const currentFile = currentUri?.fsPath;
+      const currentFolder = currentFile && dirname(currentFile);
+
+      const basePath = currentUri && vscode.workspace.getWorkspaceFolder(currentUri)?.uri.fsPath;
+      const relativeTo = path.startsWith('.') ? currentFolder : basePath;
+      const fullPath = resolve(...[relativeTo, path].filter(Boolean) as string[]);
+
+      const program = TJS.getProgramFromFiles([fullPath], compilerOptions, basePath);
+      const generator = TJS.buildGenerator(program, settings);
+      const schema = TJS.generateSchema(program, type, settings, [], generator!);
+
+      const relativePath = vscode.workspace.asRelativePath(fullPath);
+      watchFiles[relativePath] = uri;
+
+      return JSON.stringify(schema);
+    }
+  };
+
+  subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(customScheme, tsschemaProvider));
+  subscriptions.push(vscode.workspace.onDidSaveTextDocument(onSave));
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
